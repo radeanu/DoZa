63480c77c953ad5c01ddb6b74645ff656f59f6ef
radeanu <radeanu@mail.com>
2020-08-09 17:17:02 +0300

    ignore server

--- node_server/data.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  cars: [
-    { name: "BMW", color: "black", price: 140 },
-    { name: "Opel", color: "red", price: 40 },
-    { name: "Audi", color: "white", price: 130 },
-  ],
-};
