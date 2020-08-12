b10fba29812ed2bafbcaf6a9c6752b74672f7b8c
radeanu <radeanu@mail.com>
2020-08-12 23:00:18 +0300

    preference component

--- src/app/routes/dashboard/preferences/preferences.module.ts
+++ src/app/routes/dashboard/preferences/preferences.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatRadioModule } from '@angular/material/radio';
+import { Ng5SliderModule } from 'ng5-slider';
+import { CheckboxModule } from 'primeng/checkbox';
 
 import { PreferencesComponent } from './preferences.component';
 
 @NgModule({
   declarations: [PreferencesComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, MatRadioModule, Ng5SliderModule, CheckboxModule],
   exports: [PreferencesComponent],
 })
-export class AppPreferencesModule {}
+export class AppPreferencesModule { }
