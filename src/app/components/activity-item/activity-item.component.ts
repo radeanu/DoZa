7b59721a371a2f2bc0342530ee0356eb7457c519
radeanu <radeanu@mail.com>
2020-09-16 13:14:50 +0300

    added animation for activity items

--- src/app/components/activity-item/activity-item.component.ts
+++ src/app/components/activity-item/activity-item.component.ts
@@ -1,9 +1,35 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-activity-item',
   templateUrl: './activity-item.component.html',
   styleUrls: ['./activity-item.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          height: '50px',
+          padding: '5px',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          height: 0,
+          padding: 0,
+        })
+      ),
+      transition('open <=> closed', [animate('0.2s')]),
+    ]),
+  ],
 })
 export class ActivityItemComponent {
   showContent = false;
